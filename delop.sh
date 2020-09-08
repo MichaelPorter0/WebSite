eb5206dc0619d0aac1d5f629b94f5ca6c5f3d000
miporterg <miporterg@gmail.com>
2020-09-08 20:42:14 +0800

    init

--- delop.sh
+++
@@ -1,6 +0,0 @@
-hexo generate
-cp -R public/* .deploy/MichaelPorter0.github.io
-cd .deploy/MichaelPorter0.github.io
-git add .
-git commit -m'update'
-git push origin master
---
+++ source/about/index.md
@@ -0,0 +1,6 @@
+---
+title: about
+date: 2020-09-08 16:54:55
+layout: about
+---
+# 我是大哥
